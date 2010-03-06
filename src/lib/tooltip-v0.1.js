9b47b651d661e413e8595e9ce90dd9079b48ea78
sasa1977 <sasa.juric@gmail.com>
2010-03-06 14:03:01 +0100

    added timeout logic to tooltips

--- src/lib/tooltip-v0.1.js
+++ src/lib/tooltip-v0.1.js
@@ -60,73 +60,96 @@ Tooltip.prototype = {
     var options = Object.extend({
       default_css: false,
       margin: "0px",
-	  padding: "5px",
-	  backgroundColor: "#d6d6fc",
-	  delta_x: 5,
-	  delta_y: 5,
+	    padding: "5px",
+	    backgroundColor: "#d6d6fc",
+	    delta_x: 5,
+	    delta_y: 5,
       zindex: 1000
     }, arguments[1] || {});
 
     this.element      = $(element);
     this.tool_tip     = $(tool_tip);
+    
+    this.delayExecute = new Twump.Utils.DelayExecute(2000);
 
     this.options      = options;
 
     // hide the tool-tip by default
     this.tool_tip.hide();
 
-    this.eventMouseOver = this.showTooltip.bindAsEventListener(this);
-    this.eventMouseOut   = this.hideTooltip.bindAsEventListener(this);
-
+    this.eventMouseOver = function(event){
+      if (this.autohider){
+        clearTimeout(this.autohider);
+        this.autohider = null;
+      }
+    
+      this.hideTooltip(event);
+      this.delayExecute.cancel();
+      
+      this.delayExecute.schedule(function(){
+        this.showTooltip(event);
+        
+        this.autohider = setTimeout(function(){
+          this.hideTooltip(event)
+        }.bind(this), 5000)
+        
+      }.bind(this))
+    }.bind(this)
+    
+    this.eventMouseOut = function(event){
+      this.delayExecute.cancel();
+      this.hideTooltip(event);
+    }.bind(this)
+    
     this.registerEvents();
   },
 
   destroy: function() {
-    Event.stopObserving(this.element, "mouseover", this.eventMouseOver);
+    Event.stopObserving(this.element, "mousemove", this.eventMouseOver);
     Event.stopObserving(this.element, "mouseout", this.eventMouseOut);
   },
 
   registerEvents: function() {
-    Event.observe(this.element, "mouseover", this.eventMouseOver);
+    Event.observe(this.element, "mousemove", this.eventMouseOver);
     Event.observe(this.element, "mouseout", this.eventMouseOut);
   },
 
   showTooltip: function(event){
-	Event.stop(event);
-	// get Mouse position
+	  Event.stop(event);
+	  
+	  // get Mouse position
     var mouse_x = Event.pointerX(event);
-	var mouse_y = Event.pointerY(event);
+	  var mouse_y = Event.pointerY(event);
 	
 	
-	// decide if wee need to switch sides for the tooltip
-	var dimensions = Element.getDimensions( this.tool_tip );
-	var element_width = dimensions.width;
-	var element_height = dimensions.height;
+	  // decide if wee need to switch sides for the tooltip
+	  var dimensions = Element.getDimensions( this.tool_tip );
+	  var element_width = dimensions.width;
+	  var element_height = dimensions.height;
 	
-	if ( (element_width + mouse_x) >= ( this.getWindowWidth() - this.options.delta_x) ){ // too big for X
-		mouse_x = mouse_x - element_width;
-		// apply delta to make sure that the mouse is not on the tool-tip
-		mouse_x = mouse_x - this.options.delta_x;
-	} else {
-		mouse_x = mouse_x + this.options.delta_x;
-	}
+	  if ( (element_width + mouse_x) >= ( this.getWindowWidth() - this.options.delta_x) ){ // too big for X
+		  mouse_x = mouse_x - element_width;
+		  // apply delta to make sure that the mouse is not on the tool-tip
+		  mouse_x = mouse_x - this.options.delta_x;
+	  } else {
+		  mouse_x = mouse_x + this.options.delta_x;
+	  }
 	
-	if ( (element_height + mouse_y) >= ( this.getWindowHeight() - this.options.delta_y) ){ // too big for Y
-		mouse_y = mouse_y - element_height;
-	    // apply delta to make sure that the mouse is not on the tool-tip
-		mouse_y = mouse_y - this.options.delta_y;
-	} else {
-		mouse_y = mouse_y + this.options.delta_y;
-	} 
+	  if ( (element_height + mouse_y) >= ( this.getWindowHeight() - this.options.delta_y) ){ // too big for Y
+		  mouse_y = mouse_y - element_height;
+	      // apply delta to make sure that the mouse is not on the tool-tip
+		  mouse_y = mouse_y - this.options.delta_y;
+	  } else {
+		  mouse_y = mouse_y + this.options.delta_y;
+	  } 
 	
-	// now set the right styles
-	this.setStyles(mouse_x, mouse_y);
+	  // now set the right styles
+	  this.setStyles(mouse_x, mouse_y);
 	
 		
-	// finally show the Tooltip
-	//new Effect.Appear(this.tool_tip);
-	new Element.show(this.tool_tip);
-
+	  // finally show the Tooltip
+	  //new Effect.Appear(this.tool_tip);
+	  new Element.show(this.tool_tip);
   },
   
   setStyles: function(x, y){
--- src/lib/twump/utils.js
+++ src/lib/twump/utils.js
@@ -3,24 +3,42 @@ Twump.Utils.DelayExecute = Class.create()
 Twump.Utils.DelayExecute.prototype = {
   initialize: function(delay){
     this.delay = delay || 5000;
-    this.currentStep = 0;
+    this.delayedExecutors = []
   },
 
   schedule: function(callback){
-    var currentStep = this.currentStep++;
-    setTimeout(function(){this.execute(currentStep, callback)}.bind(this), this.delay)
+    var step = this.delayedExecutors.length;
+    
+    this.delayedExecutors.push(
+      setTimeout(function(){this.execute(step, callback)}.bind(this), this.delay)
+    )
   },
 
-  execute: function(currentStep, callback){
-    if (!this.cancelled(currentStep))
+  execute: function(step, callback){
+    if (!this.cancelled(step))
       callback();
+      
+    if (this.lastStep(step)) {
+      this.currentStep = 0;
+      this.delayedExecutors = [];
+    }
   },
 
-  cancelled: function(currentStep) {
-    return this.cancelledAt && this.cancelledAt >= currentStep;
+  cancelled: function(step) {
+    return this.cancelledAt && this.cancelledAt >= step;
   },
 
   cancel: function(){
-    this.cancelledAt = this.currentStep++;
+    this.cancelledAt = this.delayedExecutors.length - 1;
+    
+    for (var i = 0, length = this.delayedExecutors.length;i < length;i++){
+      var timeout = this.delayedExecutors[i];
+      if (timeout != null)
+        clearTimeout(timeout);
+    }
+  },
+  
+  lastStep: function(step){
+    return step == this.delayedExecutors.length - 1;
   }
 }
