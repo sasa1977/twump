b3310ddffc95a2e05f7afe40f342fc630c50648d
sasa1977 <sasa.juric@gmail.com>
2010-03-29 22:03:28 +0200

    changed playing finished detection

--- src/lib/twump/player.js
+++ src/lib/twump/player.js
@@ -8,11 +8,8 @@ Twump.Player.prototype = {
   },
   
   play: function(position){
-    this.soundCompleteDelegate = this.onPlaybackComplete.bind(this);
-    
     this.channel = this.sound.play(position || 0, 0, new air.SoundTransform(this.options.volume || 0.5, 0))
-    this.channel.addEventListener(air.Event.SOUND_COMPLETE, this.soundCompleteDelegate);
-    this.timer = setInterval(this.monitorPlaying.bind(this), 500);
+    this.startMonitorPlaying();
   },
   
   pauseOrResume: function(){
@@ -39,11 +36,9 @@ Twump.Player.prototype = {
   stop: function(){
     if (!this.playing()) return;
     
-    clearInterval(this.timer);
+    this.stopMonitorPlaying();
     this.channel.stop();
-    this.channel.removeEventListener(air.Event.SOUND_COMPLETE, this.soundCompleteDelegate)
     this.channel = null;
-    this.timer = null;
   },
   
   destruct: function(){
@@ -56,9 +51,63 @@ Twump.Player.prototype = {
     }
   },
   
+  startMonitorPlaying: function(){
+    if (this.timer) return;
+    this.timer = setInterval(this.monitorPlaying.bind(this), 500);
+    this.lastMonitoredPosition = null;
+    this.notChangedTimes = 0;
+  },
+  
+  stopMonitorPlaying: function(){
+    if (!this.timer) return;
+    this.timer = null;
+    this.lastMonitoredPosition = null;
+    clearInterval(this.timer);
+  },
+  
+  isPlayingFinished: function() {
+    // SOUND_COMPLETE event in air sometimes fires too early, sometimes doesn't even fire. Therefore
+    // I do this heuristic.
+    
+    // 1. if the song is longer than one minute, and we're at the end -> finish.
+    //
+    // The one minute check is because the song is loading simultaneously as it is being played. 
+    // Therefore it might happen at the beginning that the position equals the length of currently loaded part. 
+    // To avoid false end detection, I don't use this condition in the first minute of playing.
+    if (this.channel.length >= 60000 && this.channel.position >= this.sound.length)
+      return true;
+    
+    // 2. If I repeatedly don't notice position change -> finish.
+    //
+    // This part handles the situation when playing is stuck before the end of the song, and also
+    // to handle the songs shorter than one minute (see comment above)
+    if (!this.lastMonitoredPositionChanged())
+      this.notChangedTimes++;
+    else
+      this.notChangedTimes = 0;
+
+    // I return true (playing finished) only when the position didn't change for couple of successive cheks
+    // to avoid false detection when the user changes playing position. Otherwise it might happen that the user 
+    // changes to the position which is the same as the last detected, and I mistakenly detect no position change.
+    return this.notChangedTimes >= 4;
+  },
+  
+  lastMonitoredPositionChanged: function(){
+    return (!this.lastMonitoredPosition) || (this.lastMonitoredPosition != this.channel.position);
+  },
+  
   monitorPlaying: function(){
     if (!this.playing()) return;
     
+    if (!this.isPlayingFinished())
+      this.fireProgress();
+    else
+      this.onPlaybackComplete();
+  },
+  
+  fireProgress: function(){
+    this.lastMonitoredPosition = this.channel.position;
+
     var estimatedLength = Math.ceil(this.sound.length / (this.sound.bytesLoaded / this.sound.bytesTotal)); 
     this.invokeCallback("PlayProgress", {
       playbackPercent: this.playbackPercent(), 
